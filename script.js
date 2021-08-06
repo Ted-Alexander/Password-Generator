268ed0d3b58e5c255d73c4f8733e31a5a758d06f
Ted Alexander <tedalexander93@gmail.com>
2021-08-06 15:19:45 -0400

    set up functions

--- script.js
+++ script.js
@@ -6,38 +6,100 @@ upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"] //10
 symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"] //29
 
-useLower
-useUpper
-useNumbers
-useSymbols
-howLong
-generated = [];
+useLower = false;
+useUpper = false;
+useNumbers = false;
+useSymbols = false;
+lastUsed = "Z" // prevents repeats
+generated = []; //password goes here
 
-if (generated.length < howLong) {
-    generatePassword();
-}else {writePassword();
+
+function generatePassword(){
+    var howLong = prompt("How many characters would you like your password to contain?");
+      while (howLong < 8 || howLong > 127){
+        alert("Password length must be between 8-128 characters Try again");
+        var howLong = prompt(
+          "How many characters would you like your password to contain?"
+        );
+      }
+     
+      alert(`Your password will have ${howLong} characters`);
+      
+      useSymbols = confirm(
+        "Click OK to confirm if you would like to include special characters"
+      );
+      useNumbers = confirm(
+        "Click OK to confirm if you would like to include numeric characters"
+      );
+      useLower = confirm(
+        "Click OK to confirm if you would like to include lowercase characters"
+      );
+      useUpper = confirm(
+        "Click OK to confirm if you would like to include uppercase characters"
+      );
+
+      function pickCharacters() {
+        if (useLower = true && generated.length < howLong && lastUsed != "L"){
+          pickLower = Math.floor(Math.random() * lowerCase.length); //generate random number between 1-26
+          generated.push(lowerCase[pickLower]) // to be in generated array
+          lastUsed = "L" //prevent repeats
+        }
+        else if (useUpper = true && generated.length < howLong && lastUsed != "U"){
+          pickUpper = Math.floor(Math.random() * upperCase.length);  //generate random number between 1-26
+          generated.push(upperCase[pickUpper])// to be in generated array
+          lastUsed = "U"
+        }
+        else if (useNumbers = true && generated.length < howLong && lastUsed != "N"){
+          pickNumber = Math.floor(Math.random() * lowerCase.length);  //generate random number between 1-10
+          generated.push(numbers[pickNumber]) // to be in generated array
+          lastUsed = "N"
+        }
+        else if (useSymbols = true && generated.length < howLong && lastUsed != "S"){
+          pickSymbol = Math.floor(Math.random() * lowerCase.length);   //generate random number between 1-29
+          generated.push(symbols[pickSymbol]) // to be in generated array
+          lastUsed = "S"
+    
+        }
+        longEnough();
+    }
+        function longEnough(){
+          if (generated.length < howLong) {
+            pickCharacters();
+        }else {writePassword();
+}
+
+
+
+
+// function longEnough(){
+//   if (generated.length < howLong) {
+//     pickCharacters();
+// }else {writePassword();
 
 };
-
-function generatePassword() {
-    if (useLower = true && generated.length < howLong){
-      //generate random number between 1-26
-      //push lowerCase[random] to be in generated array
-    }
-    if (useUpper = true && generated.length < howLong){
-      //generate random number between 1-26
-      //push lowerCase[random] to be in generated array
-    }
-    if (useNumbers = true && generated.length < howLong){
-      //generate random number between 1-10
-      //push lowerCase[random] to be in generated array
-    }
-    if (useSymbols = true && generated.length < howLong){
-      //generate random number between 1-29
-      //push lowerCase[random] to be in generated array
-
-    }
 }
+
+
+// function pickCharacters() {
+//     if (useLower = true && generated.length < howLong){
+//       pickLower = Math.floor(Math.random() * lowerCase.length); //generate random number between 1-26
+//       generated.push(lowerCase[pickLower]) // to be in generated array
+//     }
+//     if (useUpper = true && generated.length < howLong){
+//       pickUpper = Math.floor(Math.random() * upperCase.length);  //generate random number between 1-26
+//       generated.push(upperCase[pickUpper])// to be in generated array
+//     }
+//     if (useNumbers = true && generated.length < howLong){
+//       pickNumber = Math.floor(Math.random() * lowerCase.length);  //generate random number between 1-10
+//       generated.push(numbers[pickNumber]) // to be in generated array
+//     }
+//     if (useSymbols = true && generated.length < howLong){
+//       pickSymbol = Math.floor(Math.random() * lowerCase.length);   //generate random number between 1-29
+//       generated.push(symbols[pickSymbol]) // to be in generated array
+
+//     }
+//     longEnough();
+// }
 
 
 // Write password to the #password input
